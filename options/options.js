8d1bb6d88e282a913b9eb5f869b7789df6da5a23
Crowdbotics <engineering@crowdbotics.com>
2023-07-01 00:45:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-sun-dev-96413",
+  url: "https://bitter-sun-dev-96413.botics.co",
+  api: "https://bitter-sun-dev-96413.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
